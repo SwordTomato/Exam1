a928436336599ee2bdc252101444da2d7508efe8
Sword <swordacegaming@gmail.com>
2023-03-13 16:35:37 +0700

    เลือกตั้ง4

--- Program.cs
+++ Program.cs
@@ -18,15 +18,25 @@
          Console.Write("ความสูงของประกาศฉบับที่ 2 : ");
          int h2 = int.Parse(Console.ReadLine());
 
-         int TopRight1 = x1 + w1 + h1;
-         int TopLeft1 = x1 -w1 + h1;
-         int BotRight1 = x1 + w1 - h1;
-         int BotLeft1 = x1 - w1 - h1;
-         int TopRight2 = x2 + w2 + h2;
-         int TopLeft2 = x2 -w2 + h2;
-         int BotRight2 = x2 + w2 - h2;
-         int BotLeft2 = x2 - w2 - h2;
- 
+         int TopRightx1 = x1 + w1 + h1;
+         int TopLeftx1 = x1 -w1 + h1;
+         int BotRightx1 = x1 + w1 - h1;
+         int BotLeftx1 = x1 - w1 - h1;
+         int TopRightx2 = x2 + w2 + h2;
+         int TopLeftx2 = x2 -w2 + h2;
+         int BotRightx2 = x2 + w2 - h2;
+         int BotLeftx2 = x2 - w2 - h2;
+         
+         int TopRighty1 = y1 + w1 + h1;
+         int TopLefty1 = y1 -w1 + h1;
+         int BotRighty1 = y1 + w1 - h1;
+         int BotLefty1 = y1 - w1 - h1;
+         int TopRighty2 = y2 + w2 + h2;
+         int TopLefty2 = y2 -w2 + h2;
+         int BotRighty2 = y2 + w2 - h2;
+         int BotLefty2 = y2 - w2 - h2;
+
+        if
 Console.WriteLine("จงมีความสุขกาย สุขใจ รักษาตนให้พ้นจากทุกข์ภัยทั้งสิ้นเถิด");
 
     }
