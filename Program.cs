79c3b87f9a537fe7e433f1902ca11862d12229f3
Sword <swordacegaming@gmail.com>
2023-03-13 16:33:27 +0700

    เลือกตั้ง3

--- Program.cs
+++ Program.cs
@@ -17,6 +17,15 @@
          int w2 = int.Parse(Console.ReadLine());
          Console.Write("ความสูงของประกาศฉบับที่ 2 : ");
          int h2 = int.Parse(Console.ReadLine());
+
+         int TopRight1 = x1 + w1 + h1;
+         int TopLeft1 = x1 -w1 + h1;
+         int BotRight1 = x1 + w1 - h1;
+         int BotLeft1 = x1 - w1 - h1;
+         int TopRight2 = x2 + w2 + h2;
+         int TopLeft2 = x2 -w2 + h2;
+         int BotRight2 = x2 + w2 - h2;
+         int BotLeft2 = x2 - w2 - h2;
  
 Console.WriteLine("จงมีความสุขกาย สุขใจ รักษาตนให้พ้นจากทุกข์ภัยทั้งสิ้นเถิด");
 
